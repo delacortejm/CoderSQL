5f62daf72c7b7ee545d190bce351fcc1ff9a5a67
delacortejm <delacortejm@gmail.com>
2025-05-12 20:41:31 -0300

    corrección de errores

--- vistas/vw_modelos_vendidos.sql
+++
@@ -1,4 +0,0 @@
--- Muestra los modelos de autos más vendidos, incluyendo la marca, el modelo y el total de ventas.  
--- Busca ayudar al equipo analítico a identificar los modelos más populares para informar decisiones de compra de inventario.  
-
-
--- vistas/vw_ventas_por_estado.sql
+++
@@ -1,4 +0,0 @@
---Resume las ventas realizadas por estado, mostrando el número total de ventas y el precio promedio de venta. 
---Proporciona al equipo logístico una visión clara de la distribución geográfica de las ventas para optimizar la planificación de inventario por región.  
-
-
